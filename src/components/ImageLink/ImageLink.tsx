a5a6fd45f7acbf86c17cbede99236b6563ec747d
YosmanProgramer <yosman.higuita028@gmail.com>
2022-06-23 10:47:01 -0500

    add sass file

--- src/components/ImageLink/ImageLink.tsx
+++ src/components/ImageLink/ImageLink.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { ImageLinkProps } from './ImageLink.types'
-import './ImageLink.scss'
+import '../../sass/main.scss'
 
 const ImageLink: React.FC<ImageLinkProps> = ({ src, href, target = '_self', label }) => {
   return (
-  <div className='fnd-ui-imagelink-container'>
-    <div className='fnd-ui-imagelink-panel'>
-        <img className='fnd-ui-imagelink-image' src={src} alt={`${label} image`} />
-        <a className='fnd-ui-imagelink-link' href={href} target={target}>
+  <div className='fnd-ui-imagelink__container'>
+    <div className='fnd-ui-imagelink__panel'>
+        <img className='fnd-ui-imagelink__image' src={src} alt={`${label} image`} />
+        <a className='fnd-ui-imagelink__link' href={href} target={target}>
             &#10230;
         </a>
     </div>
-    <p className='fnd-ui-imagelink-label'>
+    <p className='fnd-ui-imagelink__label'>
         {label}
     </p>
   </div>
