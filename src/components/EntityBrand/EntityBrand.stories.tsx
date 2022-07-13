e006916fe75336bc3d010a6e0562f7dfa953466c
Yosman Andres Higuita Gonzalez <73600611+YosmanProgramer@users.noreply.github.com>
2022-07-13 16:06:53 -0500

    Create EntityBrand. Close #3

---
+++ src/components/EntityBrand/EntityBrand.stories.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/function-component-definition */
+import EntityBrand from '.'
+
+export default {
+  title: 'EntityBrand',
+  component: EntityBrand
+}
+
+export const primary = () => <EntityBrand src='https://cdnimg.bnamericas.com/XLAGyDszzoiuDabXQcxEuTaYCxPcyJUrnTsWIIRrxWXzZdMBTkUPqSwGkpsoPcAe.png' />
